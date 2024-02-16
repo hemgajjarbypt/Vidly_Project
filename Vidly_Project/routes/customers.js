3d632752da2ac9c0fed7db492e1dd1d29d861a7a
hemgajjarbypt <hem.gajjar@bypeopletechnologies.com>
2024-02-16 18:59:54 +0530

    adding status

--- Vidly_Project/routes/customers.js
+++ Vidly_Project/routes/customers.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         if (result.length === 0) {
             return res.status(400).send('Customer is Not Exists!');
         }
-        return res.send(result);
+        return res.status(200).send(result);
     } catch (error) {
         return res.status(500).send('Something failed!');
     }
