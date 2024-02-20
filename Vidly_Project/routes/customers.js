988b1f1318b0404ca49f57fa790b23fdca47cd41
hemgajjarbypt <hem.gajjar@bypeopletechnologies.com>
2024-02-20 12:47:20 +0530

    updating api of project

--- Vidly_Project/routes/customers.js
+++ Vidly_Project/routes/customers.js
@@ -5,8 +5,11 @@ const mongoose = require('mongoose');
 const router = express.Router();
 router.use(express.json());
 
+
+
 router.get('/', async (req, res) => {
     try {
+        // console.log(req.query.search);
         const result = await Customer.find().sort('name').select('_id name email phone isValid');
         if (result.length === 0) {
             return res.status(400).send('Customer is Not Exists!');
@@ -17,9 +20,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+
 router.get('/:name', async (req, res) => {
     try {
         const result = await Customer.find({ name: req.params.name });
+        if (result.length === 0) {
+            return res.status(400).send('Following Customer is Not Available in Database');
+        }
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.status(500).send('Something failed!');
+    }
+});
+
+router.get('/getById/:id', async (req, res) => {
+    try {
+        console.log("req.params.id-> ", req.params.id);
+        const result = await Customer.findById(req.params.id);
         if (result.length === 0) {
             return res.status(400).send('Following Customer is Not Available in Database');
         }
@@ -56,17 +73,37 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:email', async (req, res) => {
+// router.put('/:email', async (req, res) => {
+//     try {
+//         const filter = { email: req.params.email };
+//         const update = {
+//             phone: req.body.phone,
+//             isValid: req.body.isValid
+//         };
+//         const option = { new: true };
+//         const upCustomer = await Customer.findOneAndUpdate(filter, update, option);
+//         if (!upCustomer) {
+//             return res.status(400).send("Provided Customer is Not in the Database.");
+//         }
+//         else {
+//             return res.status(200).send(_.pick(upCustomer, ['_id', 'name', 'email', 'phone', 'isValid']));
+//         }
+//     } catch (error) {
+//         return res.status(500).send('Something failed!');
+//     }
+// });
+
+router.put('/:id', async (req, res) => {
     try {
-        const filter = { email: req.params.email };
+        const filter = { _id: req.params.id };
         const update = {
-            email: req.body.email,
+            name: req.body.name,
             email: req.body.email,
             phone: req.body.phone,
             isValid: req.body.isValid
         };
         const option = { new: true };
-        const upCustomer = await Customer.findOneAndUpdate(filter, update, option);
+        const upCustomer = await Customer.findByIdAndUpdate(filter, update, option);
         if (!upCustomer) {
             return res.status(400).send("Provided Customer is Not in the Database.");
         }
@@ -77,7 +114,6 @@ router.put('/:email', async (req, res) => {
         return res.status(500).send('Something failed!');
     }
 });
-
 
 router.delete('/:email', async (req, res) => {
     try {
